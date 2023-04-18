f5bb983200cff2dee90d308f7caed87ec0132bcf
Dipo Desmond-eke <dipodesmond-eke@Dipos-MacBook-Pro.local>
2023-04-18 14:43:30 +0100

    office

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import  Logo2 from '../assets/Logo2.png'
 const NavBar = () => {
   return (
     <div>
-      
+      <div>
+      <img src={Logo2} alt=''/>
+      </div>
     </div>
   )
 }
